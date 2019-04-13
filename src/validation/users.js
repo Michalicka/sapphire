7a1a4479cdde9546ebc6c30701fc139618850009
Lubos <michalicka@pixelmate.cz>
2019-04-13 20:18:12 +0200

    create validation for postUsers

---
+++ src/validation/users.js
@@ -0,0 +1,14 @@
+
+import * as Yup from 'yup'
+import messages from '../validationMessages'
+
+export const PostUsers = Yup.object().shape({
+  displayName: Yup.string()
+    .required(messages.required),
+  email: Yup.string()
+    .email(messages.email)
+    .required(messages.required),
+  password: Yup.string()
+    .min(6, messages.min)
+    .required(messages.required)
+})
---
+++ src/validationMessages.js
@@ -0,0 +1,6 @@
+
+export default {
+  required: 'Field is required',
+  email: 'Field has to be an email',
+  min: `Field must be at least 6 characters long`
+}
