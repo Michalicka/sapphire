5437562737e4c079224be2f975e4736fda440c1f
Lubos <michalicka@pixelmate.cz>
2019-04-13 20:19:27 +0200

    add registation route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 
 import React from 'react'
 import withRoot from './withRoot'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import rootReducer from './reducers'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Registration from './views/Registration'
+import { registration } from './routes'
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
 
 export class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-        </Router>
-      </Provider>
+      // <Provider store={store}>
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => <Redirect to={registration} />}
+        />
+        <Route path={registration} component={Registration} />
+      </Router>
+      // </Provider>
     )
   }
 }
