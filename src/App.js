7664db748ede5ad36bd21f707ad8df3e7555e230
Lubos <michalicka@pixelmate.cz>
2019-04-28 15:03:00 +0200

    add redux store provider to app.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import withRoot from './withRoot'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Registration from './views/Registration'
 import { registration } from './routes'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import rootReducer from './reducers'
+import rootSaga from './sagas'
 
-// const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
 
 export class App extends React.Component {
   render() {
     return (
-      // <Provider store={store}>
-      <Router>
-        <Route
-          exact
-          path="/"
-          render={() => <Redirect to={registration} />}
-        />
-        <Route path={registration} component={Registration} />
-      </Router>
-      // </Provider>
+      <Provider store={store}>
+        <Router>
+          <Route
+            exact
+            path="/"
+            render={() => <Redirect to={registration} />}
+          />
+          <Route path={registration} component={Registration} />
+        </Router>
+      </Provider>
     )
   }
 }
